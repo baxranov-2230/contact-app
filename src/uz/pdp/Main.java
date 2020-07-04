774513fd673cd4b95fc77bcbf7421087097548b5
baxranov-2230 <baxranovahror2230@gmail.com>
2020-07-04 19:59:32 +0500

    77

--- src/uz/pdp/Main.java
+++ src/uz/pdp/Main.java
@@ -12,7 +12,7 @@ public class Main {
         int chance = -1;
 
         while (chance != 0) {
-            System.out.println("000=>Exit, 1=>addContact, 2=>printContact, 3=>editContact, 4=>deleteContact, 5=>searchContact");
+            System.out.println("0=>Exit, 1=>addContact, 2=>printContact, 3=>editContact, 4=>deleteContact, 5=>searchContact");
             chance = scanner.nextInt();
             switch (chance) {
                 case 1:
